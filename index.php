ff5845fce5ae53d5214bece09529db66bab3efeb
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-04-17 14:13:48 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+/* Descrizione
+
+Dobbiamo creare una web-app che permetta di leggere una lista di dischi presente nel nostro server.
+
+Stack
+
+Html, CSS, VueJS (importato tramite CDN), axios, PHP
+
+Consigli
+
+Nello svolgere l’esercizio seguite un approccio graduale.
+Prima assicuratevi che la vostra pagina index.php (il vostro front-end) riesca a comunicare correttamente con il vostro script PHP (le vostre “API”).
+Solo a questo punto sarà utile passare alla lettura della lista da un file JSON.
+
+Bonus
+
+Al click su un disco, recuperare e mostrare i dati del disco selezionato.
+ */
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP disks</title>
+</head>
+<body>
+    
+</body>
+</html>
